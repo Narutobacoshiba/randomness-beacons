5314474cb2da2d020df86fa12d0e12754369b696
Narutobacoshiba <haphapbk29@gmail.com>
2023-01-18 17:53:20 +0700

    daily update 01/18/2023 1

--- random_org_bot.mjs
+++ random_org_bot.mjs
@@ -10,6 +10,7 @@ import { assert, sleep} from "@cosmjs/utils"
 import { toUtf8 } from "@cosmjs/encoding"
 import crypto from "crypto";
 import chalk from "chalk"
+import { rmSync } from "fs"
 
 dotenv.config();
 
@@ -30,7 +31,7 @@ assert(process.env.AURAND_CONTRACT, "AURAND_CONTRACT must be set");
 const aurandContract = process.env.AURAND_CONTRACT;
 assert(process.env.GAS_PRICE, "GAS_PRICE must be set. E.g. '0.025unois'");
 const gasPrice = GasPrice.fromString(process.env.GAS_PRICE);
-
+const gasWanted = parseInt(process.env.GAS_WANTED)
 //random org api key
 assert(process.env.API_KEY, "API_KEY must be set");
 const api_key = process.env.API_KEY;
@@ -164,75 +165,94 @@ function isSet(a) {
     return a !== null && a !== undefined;
 }
 
+let broadcaster2 = endpoint2 ? await CosmWasmClient.connect(endpoint2) : null;
+let broadcaster3 = endpoint3 ? await CosmWasmClient.connect(endpoint3) : null;
+
+await resetSignData();
+
 async function main() {
+    console.info(infoColor("Waiting for commitments ..."))
+    const QueryNumberOfCommitment = {
+        get_number_of_commitment: {}
+    }
+    const res = await client.queryContractSmart(
+        aurandContract, 
+        QueryNumberOfCommitment
+    )
+    
+    if (res && res.num > 0) {
+        console.info(infoColor("Detect commitments ..."))
+        console.info(infoColor("Get random org randomness ..."));
+        const random_value = await getSignedInteger({api_key: api_key, min: 0, max: 255, amount: 32 })
+        console.info(successColor(`Receive random value with completion time: ${random_value.random.completionTime}`))
 
-    let broadcaster2 = endpoint2 ? await CosmWasmClient.connect(endpoint2) : null;
-    let broadcaster3 = endpoint3 ? await CosmWasmClient.connect(endpoint3) : null;
+        try {
+            const msg = {
+                typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
+                value: MsgExecuteContract.fromPartial({
+                    sender: botAddress,
+                    contract: aurandContract,
+                    msg: toUtf8(
+                        JSON.stringify({
+                            add_randomness: {
+                                random_value: JSON.stringify(random_value.random),
+                                signature: random_value.signature
+                            },
+                        }),
+                    ),
+                }),
+            };
+            
+            const memo = "Bot add randomness";
+            const signData = getNextSignData(); // Do this the manual way to save one query
 
-    await resetSignData();
+            let usedFee
+            if (gasWanted) {
+                usedFee = calculateFee(gasWanted, gasPrice);
+            } else {
+                const gasEstimation = await client.simulate(botAddress, [msg], memo);
+                const multiplier = 1.8;
+                usedFee = calculateFee(Math.round(gasEstimation * multiplier), gasPrice);
+            }
+            
+            const signed = await client.sign(botAddress, [msg], usedFee, memo, signData);
+            const tx = Uint8Array.from(TxRaw.encode(signed).finish());
 
-    console.info(infoColor("Get random org randomness ..."));
-    const random_value = await getSignedInteger({api_key: api_key, min: 0, max: 255, amount: 32 })
-    console.info(successColor(`Receive random value with completion time: ${random_value.random.completionTime}`))
+            const p1 = client.broadcastTx(tx);
+            const p2 = broadcaster2?.broadcastTx(tx);
+            const p3 = broadcaster3?.broadcastTx(tx);
+            p1.then(
+                () => console.log(infoColor("Broadcast 1 succeeded")),
+                (err) => console.warn(warningColor(`Broadcast 1 failed: ${err}`)),
+            );
+            p2?.then(
+                () => console.log(infoColor("Broadcast 2 succeeded")),
+                (err) => console.warn(warningColor(`Broadcast 2 failed: ${err}`)),
+            );
+            p3?.then(
+                () => console.log(infoColor("Broadcast 3 succeeded")),
+                (err) => console.warn(warningColor(`Broadcast 3 failed: ${err}`)),
+            );
 
-    try {
-        const msg = {
-            typeUrl: "/cosmwasm.wasm.v1.MsgExecuteContract",
-            value: MsgExecuteContract.fromPartial({
-                sender: botAddress,
-                contract: aurandContract,
-                msg: toUtf8(
-                    JSON.stringify({
-                        add_randomness: {
-                            random_value: JSON.stringify(random_value.random),
-                            signature: random_value.signature
-                        },
-                    }),
+            const result = await Promise.any([p1, p2, p3].filter(isSet));
+            assertIsDeliverTxSuccess(result);
+            
+            console.info(
+                successColor(
+                `(Gas: ${result.gasUsed}/${result.gasWanted}; Block height: ${result.height}; Transaction: ${result.transactionHash})`,
                 ),
-            }),
-        };
-        
-        const memo = "Bot add randomness";
-        const signData = getNextSignData(); // Do this the manual way to save one query
-        const gasEstimation = await client.simulate(botAddress, [msg], memo);
-        const multiplier = 1.8;
-        const usedFee = calculateFee(Math.round(gasEstimation * multiplier), gasPrice);
-        const signed = await client.sign(botAddress, [msg], usedFee, memo, signData);
-        const tx = Uint8Array.from(TxRaw.encode(signed).finish());
+            )
+        } catch (e) {
+            console.error(errorColor(e.toString()));
 
-        const p1 = client.broadcastTx(tx);
-        const p2 = broadcaster2?.broadcastTx(tx);
-        const p3 = broadcaster3?.broadcastTx(tx);
-        p1.then(
-            () => console.log(infoColor("Broadcast 1 succeeded")),
-            (err) => console.warn(warningColor(`Broadcast 1 failed: ${err}`)),
-        );
-        p2?.then(
-            () => console.log(infoColor("Broadcast 2 succeeded")),
-            (err) => console.warn(warningColor(`Broadcast 2 failed: ${err}`)),
-        );
-        p3?.then(
-            () => console.log(infoColor("Broadcast 3 succeeded")),
-            (err) => console.warn(warningColor(`Broadcast 3 failed: ${err}`)),
-        );
-
-        const result = await Promise.any([p1, p2, p3].filter(isSet));
-        assertIsDeliverTxSuccess(result);
-        
-        console.info(
-            successColor(
-            `(Gas: ${result.gasUsed}/${result.gasWanted}; Block height: ${result.height}; Transaction: ${result.transactionHash})`,
-            ),
-        )
-    } catch (e) {
-        console.error(errorColor(e.toString()));
-
-        // In case of an error, reset the chain ID and sequence to the on-chain values.
-        // If this also fails, the process is killed since the error here is not caught anymore.
-        console.info(infoColor("Resetting sign data ..."));
-        await resetSignData();
+            // In case of an error, reset the chain ID and sequence to the on-chain values.
+            // If this also fails, the process is killed since the error here is not caught anymore.
+            console.info(infoColor("Resetting sign data ..."));
+            await resetSignData();
+        }
     }
 }
 
 setInterval(main, delay*1000)
 
+//aura17x8vsr9r0c7cggz75cwg0tgr24xcd26v3aj3vdznfynd5373s0fqsypjen
